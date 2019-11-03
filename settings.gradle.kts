b07ad10afc4d2dc043acc1f4365eaa59c05c9155
Lucwousin <Lucwousin@users.noreply.github.com>
2019-11-03 02:38:38 +0100

    Add license and missing (c) info

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,9 @@
+/*
+ * Copyright (c) 2019, Lucas <https://github.com/Lucwousin>
+ * All rights reserved.
+ *
+ * This code is licensed under GPL3, see the complete license in
+ * the LICENSE file in the root directory of this source tree.
+ */
 rootProject.name = "injector-plugin"
 
--- src/main/java/com/openosrs/injector/Injexception.java
+++ src/main/java/com/openosrs/injector/Injexception.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2019, Lucas <https://github.com/Lucwousin>
+ * All rights reserved.
+ *
+ * This code is licensed under GPL3, see the complete license in
+ * the LICENSE file in the root directory of this source tree.
+ */
 package com.openosrs.injector;
 
 public class Injexception extends Exception
